3a14e789dcb95b79cf0f302f41045dfedc45c583
alttdev <altingithub@gmail.com>
2022-07-19 02:18:12 +0200

    Update 1: React Page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import profileImage from "./ye.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>React Website</h1>
+       <h2>By alttdev</h2>
+       <img src={profileImage}></img>
+    </div>
+  );
+}
+
+export default App;
